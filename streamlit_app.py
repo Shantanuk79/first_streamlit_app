8a8ae69b53da8d111a6d407d9b8485e048440bac
Shantanuk79 <131394175+Shantanuk79@users.noreply.github.com>
2023-04-23 18:11:17 +0530

    Update streamlit_app.py
    Added structure

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 import pandas
 import snowflake.connector
+import requests
+from urllib.error import URLError
 
 streamlit.title('🥣 Perfect Omelette Recipe')
 streamlit.header('Ingredients Required')
@@ -28,7 +30,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # streamlit.text(fruityvice_response.json()) #only writes json on screen
 
